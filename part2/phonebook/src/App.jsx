0c84e582f6608fdc4444f30a747ba2adbb5da0e4
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-05-14 07:10:41 +0700

    finished 2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,29 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import phonebook from "./services/phonebook";
 import PersonForm from "./components/PersonForm";
+import Notification from "./components/Notification";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    phonebook.getAll().then((initialData) => {
+      setPersons(initialData);
+      console.log("retrieved data phonebook");
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
     const isDuplicate = persons.some((person) => person.name === newName);
 
     if (isDuplicate) {
-      alert(newName + " is already added to the phonebook");
-      return;
+      const confirmed = window.confirm(
+        newName +
+          " is already added to the phonebook, replace the old number with a new one?"
+      );
+      if (!confirmed) {
+        return;
+      }
+      const existingPerson = persons.find((person) => person.name === newName);
+      const changedPerson = { ...existingPerson, number: newNumber };
+      phonebook
+        .update(existingPerson.id, changedPerson)
+        .then((updated) => {
+          setPersons(
+            persons.map((person) =>
+              person.id !== existingPerson.id ? person : updated
+            )
+          );
+        })
+        .catch((error) => {
+          const messageObject = {
+            text: `Information of ${existingPerson.name} has been removed from server`,
+            type: 'error'
+          }
+          setMessage(messageObject);
+          setTimeout(() => {
+            setMessage(null);
+          }, 5000);
+        });
     }
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
     };
-    console.log("button clicked", event.target);
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+    phonebook.create(personObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
+      setNewName("");
+      setNewNumber("");
+      console.log('added')
+      const messageObject = {
+        text: `Information of ${returnedPerson.name} has been added to server`,
+        type: 'success'
+      }
+      setMessage(messageObject);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    });
   };
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -42,18 +88,38 @@ const App = () => {
         person.name.toLowerCase().includes(searchQuery.toLowerCase())
       )
     : persons;
+
+  const deletePerson = (id) => {
+    //const person = persons.find(n => n.id === id)
+    phonebook
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter((person) => person.id !== id));
+      })
+      .catch((error) => {
+        console.log("error deleting person");
+      });
+  };
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message}/>
+
       <Filter
         key={searchQuery.id}
         searchQuery={searchQuery}
         handleSearch={handleSearch}
       />
       <h2>Add new person</h2>
-      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <Persons key={filteredPersons.id} filteredPersons={filteredPersons} />
+      <Persons filteredPersons={filteredPersons} deletePerson={deletePerson} />
     </div>
   );
 };
---
+++ part2/phonebook/src/components/Notification.jsx
@@ -0,0 +1,12 @@
+const Notification = ({message}) => {
+    if (message === null) {
+      return null
+    }
+    return (
+            <div className={message.type}>
+              {message.text}
+            </div>
+          )
+   
+  }
+export default Notification;
