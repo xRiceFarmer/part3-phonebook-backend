0468aeb6357156008036738b8b2232f7a8f35b9b
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-05-17 11:33:03 +0700

    separeted phonebook backend

--- part2/countries/src/services/countries.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001'
-
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then(response => response.data)
-}
-export default {getAll}
