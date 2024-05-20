f3d2a73b663727d724fb2e08a39fa7963e5593be
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-05-20 11:08:14 +0700

    finished 3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://xRiceFarmer:${password}@cluster0.xmtcrlu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    const person = new Person({
+        name: name,
+        number: number
+    })
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
