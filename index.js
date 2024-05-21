58414bae7f804f74838aed94ab1673652280100c
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-05-21 12:16:11 +0700

    3.22

--- index.js
+++ index.js
@@ -31,15 +31,8 @@ app.get("/api/info", (request, response) => {
   });
 });
 
-const generateId = () => {
-  return Date.now() + Math.floor(Math.random() * 10000);
-};
-
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({ error: "content missing" });
-  }
   Person.findOne({ name: body.name.toLowerCase() })
     .then((existingPerson) => {
       if (existingPerson) {
@@ -57,7 +50,7 @@ app.post("/api/persons", (request, response) => {
           response.json(savedPerson);
         })
         .catch((error) => {
-          response.status(500).json({ error: "saving failed" });
+          next(error);
         });
     })
     .catch((error) => {
@@ -74,7 +67,7 @@ app.get("/api/persons/:id", (request, response, next) => {
       }
     })
     .catch((error) => {
-      next(error)
+      next(error);
     });
 });
 
@@ -90,37 +83,36 @@ app.delete("/api/persons/:id", (request, response, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-    const body = request.body
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const { name, number } = request.body;
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
-})
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 // Error handling for unknown routes
 app.use((request, response) => {
   response.status(404).json({ error: "Unknown endpoint" });
 });
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message });
-    }
-    next(error)
-}
-app.use(errorHandler)
-  
+  console.error(error.message);
 
-const PORT = process.env.PORT;
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
